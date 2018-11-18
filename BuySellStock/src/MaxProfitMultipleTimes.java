11ee210d0341500dc5d85d32515645cd7e9163f9
Sonthy <vsonthy@ebay.com>
2018-11-17 19:37:41 -0800

    BuySellStock Multiple Time Max profit algo

---
+++ BuySellStock/src/MaxProfitMultipleTimes.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MaxProfitMultipleTimes {
+
+    public MaxProfitMultipleTimes(){
+        List<Long> input = Arrays.asList(10l, 40l, 15l, 10l, 9l, 15l, 89l, 74l);
+        System.out.println(getMaxProfit(input));
+    }
+
+    public long getMaxProfit(List<Long> input){
+        boolean hasShare = false;
+        long purchasePrice = 0;
+        long profit = 0;
+        for(int i =0 ; i < input.size() ; i++){
+            if(!hasShare && i < input.size() -1 && input.get(i) < input.get(i+1)){
+                    hasShare = true;
+                    purchasePrice = input.get(i);
+
+            }else if(hasShare && (i == input.size() -1 || input.get(i) > input.get(i+1))){
+                hasShare = false;
+                profit += input.get(i) - purchasePrice;
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args){
+        MaxProfitMultipleTimes problem = new MaxProfitMultipleTimes();
+    }
+}
---
+++ BuySellStock/src/MaxProfitOnce.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MaxProfitOnce {
+
+    public MaxProfitOnce(){
+        List<Integer> input = Arrays.asList(8, 40, 15, 10, 9, 15, 89, 74);
+        System.out.println(getMaxProfit(input));
+    }
+
+    private long getMaxProfit(List<Integer> prices) {
+        if(prices.size() < 2){
+            return 0;
+        }
+        int maxProfit=0;
+        int minPrice=prices.get(0);
+        for(int i=1;i<prices.size() ; i++){
+            if(prices.get(i) < minPrice){
+                minPrice = prices.get(i);
+            } else if(maxProfit < (prices.get(i) - minPrice)){
+                maxProfit = (prices.get(i) - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+
+
+    public static void main(String[] args){
+        MaxProfitOnce problem = new MaxProfitOnce();
+    }
+}
